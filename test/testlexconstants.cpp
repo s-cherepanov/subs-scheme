410a9e3ef8ef461d86fe7e1540cb25e5253633ea
Andy Balaam <andybalaam@artificialworlds.net>
2010-07-29 02:21:04 +0100

    Add a test for lexing combinations within combinations.

--- test/testlexconstants.cpp
+++ test/testlexconstants.cpp
@@ -49,11 +49,46 @@ void combination_becomes_branch_with_leaves()
 }
 
 
+void combination_containing_combination()
+{
+    auto_ptr<StringTree> t = Lexer().Lex( "(5 (6 3) 7)" );
+
+    StringTreeBranch* lexed_branch = dynamic_cast<StringTreeBranch*>( t.get() );
+
+    TEST_ASSERT_NOT_NULL( lexed_branch );
+
+    TEST_ASSERT_EQUAL( lexed_branch->size(), 3 );
+
+    StringTreeLeaf* lexed_leaf = dynamic_cast<StringTreeLeaf*>(
+        (*lexed_branch)[0] );
+    TEST_ASSERT_NOT_NULL( lexed_leaf );
+    TEST_ASSERT_EQUAL( lexed_leaf->str(), "5" );
+
+    lexed_leaf = dynamic_cast<StringTreeLeaf*>( (*lexed_branch)[2] );
+    TEST_ASSERT_NOT_NULL( lexed_leaf );
+    TEST_ASSERT_EQUAL( lexed_leaf->str(), "7" );
+
+    lexed_branch = dynamic_cast<StringTreeBranch*>( (*lexed_branch)[1] );
+    TEST_ASSERT_NOT_NULL( lexed_branch );
+    TEST_ASSERT_EQUAL( lexed_branch->size(), 2 );
+
+    lexed_leaf = dynamic_cast<StringTreeLeaf*>( (*lexed_branch)[0] );
+    TEST_ASSERT_NOT_NULL( lexed_leaf );
+    TEST_ASSERT_EQUAL( lexed_leaf->str(), "6" );
+
+    lexed_leaf = dynamic_cast<StringTreeLeaf*>( (*lexed_branch)[1] );
+    TEST_ASSERT_NOT_NULL( lexed_leaf );
+    TEST_ASSERT_EQUAL( lexed_leaf->str(), "3" );
+}
+
+
+
 }
 
 void TestLexConstants::Run() const
 {
     bare_number_becomes_single_leaf();
     combination_becomes_branch_with_leaves();
+    combination_containing_combination();
 }
 

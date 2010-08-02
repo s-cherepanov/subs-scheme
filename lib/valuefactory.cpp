10263179cb17ad94c1eeee208d96cc00d1736822
Andy Balaam <andybalaam@artificialworlds.net>
2010-08-02 15:02:15 +0100

    Support negative integers.

--- lib/valuefactory.cpp
+++ lib/valuefactory.cpp
@@ -15,9 +15,17 @@ using namespace std;
 namespace
 {
 
-bool is_all_numbers( const string& str )
+bool is_integer( const string& str )
 {
-    for( string::const_iterator it = str.begin(); it != str.end(); ++it )
+    string::const_iterator it = str.begin();
+
+    // Minus sign is valid at the beginning
+    if( *it == '-' )
+    {
+        ++it;
+    }
+
+    for( ; it != str.end(); ++it )
     {
         char ch = *it;
         if( ch < '0' or ch > '9' )
@@ -36,7 +44,7 @@ namespace ValueFactory
 std::auto_ptr<Value> CreateValue( const StringTreeLeaf* leaf )
 {
     const std::string& leaf_str = leaf->str();
-    if( is_all_numbers( leaf_str ) )
+    if( is_integer( leaf_str ) )
     {
         // TODO: handle different sorts of int e.g larger, unsigned?
         istringstream ss( leaf_str );
--- test/testconstants.cpp
+++ test/testconstants.cpp
@@ -20,6 +20,12 @@ void bare_number_yields_itself()
 }
 
 
+void negative_number_yields_itself()
+{
+    TEST_ASSERT_EQUAL( SubsInterpreter().Interpret( "-5" ), "-5" );
+}
+
+
 void combo_yields_itself()
 {
     TEST_ASSERT_EQUAL( SubsInterpreter().Interpret( "(4 5)" ), "(4 5)" );
@@ -41,6 +47,8 @@ void TestConstants::Run() const
 {
     empty_string_yields_empty_string();
     bare_number_yields_itself();
+    negative_number_yields_itself();
+    //TODO unparseable_integer_is_an_error();
     //INCORRECT could quote combo_yields_itself();
     //INCORRECT could quote combo_of_combos_yields_itself();
 }
